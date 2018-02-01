883b323b5247f71e4edba11f2e6f6541cee10139
Pablo Aredu <pablo.adu83@gmail.com>
2018-02-01 19:37:22 +0000

    Added messages and not submit when tweet is empty or too long

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,6 @@
 * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
 function renderTweets(tweets) {
   $('#tweets').empty();
   tweets.forEach(element => {
@@ -75,7 +27,7 @@ var $replyIcon = $('<img src = "https://www.theregister.co.uk/Design/graphics/ic
 var $flagIcon = $('<img src = "https://www.theregister.co.uk/Design/graphics/icons/reply_icon.png" />');
 var $likeIcon = $('<img src = "https://www.theregister.co.uk/Design/graphics/icons/reply_icon.png" />')
 
-// Building structure of elements
+// Building structure of elements in DOM
 $header.append($avatar);
 $header.append($userName);
 $header.append($tweetUserName);
@@ -91,9 +43,46 @@ var $tweet = $($article).addClass("tweet");
 return $tweet;
 }
 
+//Loading tweets from database
+function loadTweets () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (response)=>{
+        renderTweets(response)
+      }
+  });
+}
 
+// When document ready:
 $(function() {
-  renderTweets(data);
-  // data.push(/*new tweet*/);
+  // Creating a new tweet /// QUESTION TO REMEMBER: WHY DOES THIS FUNCTION HAS TO BE ON DOCUMENT READY??
+$('#tweetform').submit(function(event){
+  event.preventDefault();
+  var newTweetText = $('#newTweet').val();
+  if (newTweetText === "" || newTweetText === null) {
+    var $messageEmptyTweet = $('<p>Your tweet is empty. Let the world knows what you are thinking!</p>');
+    $('#new-tweet').prepend($messageEmptyTweet);
+  } else if (newTweetText.length > 140) {
+    var $messageBiggerTweet = $('<p>You only have 140 characters. Please try again.</p>');
+    $('#new-tweet').prepend($messageBiggerTweet);
+  } else {
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $('form').serialize(),
+      success: function(event){
+        console.log("Ajax call was successful");
+      },
+      error: function(err){
+        console.log("There was an error calling the api ");
+        console.log(err);
+      }
+    });
+  }
 });
+  loadTweets(); //Testing GET response: DELETE LATER
+});
+
 
